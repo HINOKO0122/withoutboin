fb00e02f79fd4424bbccade5a7371462bd488edd
hinoko0122 <96116193+HINOKO0122@users.noreply.github.com>
2026-09-18 23:23:57 +0900

    Update words.js

--- words.js
+++ words.js
@@ -95,6 +95,6 @@ const WORD_DATABASE = [
 
   //別解
   "clone", "neither", "sheer", "sample", "port", "cord", "disk", "whale", "while", "wheel", "worm",
-  "paints", "peanuts", "plate", "pleat", "plot", "pilot", "proper", "slot", "trap", "bitter", "butter",
+  "paint", "peanuts", "plate", "pleat", "plot", "pilot", "proper", "slot", "trap", "bitter", "butter",
   "mist", "must", "song", "shop", "further", "feather", "escort"
 ];
